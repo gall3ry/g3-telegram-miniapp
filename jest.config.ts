4cda80c8caefa847b2d819754ea704634eb1bd3a
g3-tin <tin@platfarm.net>
2024-06-18 22:42:05 +0700

    ci: 🎡 pass with no test

--- jest.config.ts
+++ jest.config.ts
@@ -1,5 +1,8 @@
 import { getJestProjectsAsync } from '@nx/jest';
+import { Config } from 'jest';
 
-export default async () => ({
-projects: await getJestProjectsAsync()
-});
+export default async () =>
+  ({
+    projects: await getJestProjectsAsync(),
+    passWithNoTests: true,
+  } satisfies Config);
